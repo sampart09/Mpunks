e4b3d64c7b6a27e07e9417490bf22ef04fca2444
sampart09 <77251021+sampart09@users.noreply.github.com>
2022-02-14 19:21:53 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -204,14 +204,11 @@ function App() {
         image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
       >
         <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
-        <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 200 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
             <StyledImg alt={"example"} src={"/config/images/example.gif"} />
           </s.Container>
-          <s.SpacerLarge />
             <s.Container
-            flex={2}
             jc={"center"}
             ai={"center"}
           >
